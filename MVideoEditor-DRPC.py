bd6874d2f78231e3a75d552a7df8da880c0d905e
Diramix <79011730+DANBrawl@users.noreply.github.com>
2024-06-13 14:23:56 +0300

    MVideoEditor-DRPC 1.2

---
+++ MVideoEditor-DRPC.py
@@ -0,0 +1,27 @@
+from pypresence import Presence
+import time
+import pyautogui
+
+RPC = Presence(1243721758591418439)
+RPC.connect()
+
+TIME = time.time()
+
+print('MVideo-DRPC: Enabled')
+
+while True:
+    windows = pyautogui.getWindowsWithTitle("Movavi Video Editor –")
+    for window in windows:
+        if "Movavi Video Editor" in window.title:
+            pos = window.title.find('–')
+            project_name = window.title[pos + 1:]
+            del_star = project_name.find('*')
+            final_name = project_name[:del_star]
+        
+            RPC.update(
+                state="Editing –" + final_name,
+                large_image="icon",
+                large_text="Movavi Video Editor",
+                start=TIME
+            )
+        time.sleep(1)
